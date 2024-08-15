a5c5d5d10e3dc149caf0f09f6293663aba3fcc82
auro <auro@DESKTOP-AV8I9BT>
2024-08-15 14:42:17 +0300

    Add project files.

---
+++ AbstractionPractice/Employee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractionPractice
+{
+    public abstract class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Department { get; set; }
+        public string  Position { get; set; }
+
+        public abstract void Mission();  // Abstract metot
+    }
+    
+    public class Worker : Employee
+    { 
+        public Worker(string firstName, string lastName, string department, string position ) 
+        {
+            FirstName = firstName;  
+            LastName = lastName;
+            Department = department;
+            Position = position;
+        }
+        public override void Mission()          // Base classtan  gelen metotu override yaparak ezmek.
+        {
+            Console.WriteLine($"Merhaba ben {FirstName} {LastName},\n{Department} departmanında {Position} pozisyonu olarak çalışıyorum.");
+        }
+
+
+    }
+}
---
+++ AbstractionPractice/Program.cs
@@ -0,0 +1,17 @@
+﻿using AbstractionPractice;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Worker worker1 = new Worker("Ahmet", "Yılmaz", "Yazılım", "Yazılım Geliştirici");
+        worker1.Mission();
+
+        Worker worker2 = new Worker("Gizem", "Çelik", "x", "Proje Yöneticisi");
+        worker2.Mission();
+
+        Worker worker3 = new Worker("x", "y", "b", "Satış Temsilcisi");
+        worker3.Mission();
+
+    }
+}
